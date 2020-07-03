953f5f55749a2bbed9e9d312219391cea9160168
sanskriti1992 <57987584+sanskriti1992@users.noreply.github.com>
2020-07-03 23:59:11 +0530

    Add files via upload

---
+++ functions.py
@@ -0,0 +1,13 @@
+#fucntions 
+#def is used to defined the function for example 
+def display(str):
+    "Hola"
+    print(str);
+    return;
+#where display=fuction name and str=parameter 
+
+#calling the function 
+display("Yo");
+display("hi");
+display("Annyeong");
+#where yo,hi and annyeong are parameters 
